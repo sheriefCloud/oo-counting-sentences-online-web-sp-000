c0c5d6e0c7dab8a0baab4eb8dc805263a19f33b3
Sherief Farid <sherief@farid.cloud>
2019-03-23 18:37:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
 
+  def count_sentences
+    array = self.split(/\.+|!+|\?/)
+    #binding.pry
+    array.count
   end
 end
